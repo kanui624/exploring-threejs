05f39db7c4a4d81b9ea36a8a6f86e1390d07a915
Kanui <marley.kanui@gmail.com>
2020-12-17 13:08:24 -0600

    exploring threejs - global commit 6 - placed model and fixed clipping image planes

--- three-js-tutorial/pages/index.js
+++ three-js-tutorial/pages/index.js
@@ -1,11 +1,11 @@
 // React
-import { useRef, useState, Suspense } from 'react';
+import { useRef, useState, Suspense, useMemo, useEffect } from 'react';
 // Three
 import * as THREE from 'three';
 // React Three Fiber
 import { useThree, Canvas, useFrame, useLoader } from 'react-three-fiber';
 // Drei
-import { softShadows, Html, OrbitControls, useGLTF } from '@react-three/drei';
+import { softShadows, OrbitControls } from '@react-three/drei';
 
 // React Spring
 import { useSpring, a } from 'react-spring/three';
@@ -17,6 +17,17 @@ import Router from 'next/router';
 import { Geometry } from 'three';
 
 softShadows();
+
+const OControls = () => {
+  return (
+    <OrbitControls
+      enableZoom={false}
+      minPolarAngle={Math.PI / 2 - 0.5}
+      maxPolarAngle={Math.PI / 2 - 0.5}
+      autoRotate
+    />
+  );
+};
 
 const SphereMesh = ({ position, args, speed, link, name }) => {
   const text = useRef(null);
@@ -32,35 +43,36 @@ const SphereMesh = ({ position, args, speed, link, name }) => {
     scale: expand ? [0.5, 0.5, 0.5] : [0.25, 0.25, 0.25],
   });
 
-  const handlePointer = (e) => {
+  const handlePointerDown = (e) => {
     e.stopPropagation();
-    console.log(e);
+  };
+
+  const handlePointerUp = () => {
     setExpand(!expand);
     setTimeout(() => {
       Router.push(`/${link}`);
-    }, 1000);
+    }, 500);
   };
 
   const handleHover = (e, cursor) => {
     e.stopPropagation();
     if (cursor) {
-      setHovered(true);
+      setHovered(cursor);
       document.body.style.cursor = 'pointer';
     } else {
       document.body.style.cursor = 'default';
-      setHovered(false);
+      setHovered(cursor);
     }
   };
 
   return (
     <a.mesh
-      onPointerDown={(e) => handlePointer(e)}
-      // onPointerMove={(e) => null}
+      onPointerDown={(e) => handlePointerDown(e)}
+      onPointerUp={() => handlePointerUp()}
       onPointerOver={(e) => handleHover(e, true)}
       onPointerOut={(e) => handleHover(e, false)}
       scale={props.scale}
       castShadow
-      // ref={mesh}
       position={position}
     >
       <sphereBufferGeometry attach="geometry" />
@@ -68,178 +80,91 @@ const SphereMesh = ({ position, args, speed, link, name }) => {
         attach="material"
         color={hovered ? 'white' : '#bf8040'}
       />
-      {/* <Html center>
-        <p
-          onClick={(e) => handleClick(e)}
-          onPointerOver={(e) => handleHover(e, true)}
-          onPointerOut={(e) => handleHover(e, false)}
-        >
-          {name}
-        </p>
-      </Html> */}
     </a.mesh>
   );
 };
 
 const Lights = () => {
+  const dlight = useRef();
+
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <directionalLight
+      <ambientLight intensity={0.6} />
+      {/* <directionalLight
+        ref={dlight}
         castShadow
-        position={[0, 10, 0]}
-        intensity={0.3}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        position={[30, 30, 30]}
+        intensity={1}
         shadow-camera-far={50}
         shadow-camera-left={-10}
         shadow-camera-right={10}
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
-      />
-      {/* <pointLight position={[-10, 0, -20]} intensity={0.5} /> */}
-      <pointLight position={[0, -10, -20]} intensity={0.2} />
+      /> */}
+      {/* <pointLight position={[30, 0, 0]} intensity={0.1} /> */}
+      {/* <pointLight position={[5, -10, 0]} intensity={0.2} /> */}
     </>
   );
 };
 
+const Shadow = () => {
+  return (
+    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <shadowMaterial attach="material" opacity={0.4} />
+    </mesh>
+  );
+};
+
+const Group = () => {
+  return (
+    <group position={[0, 0, 0]}>
+      <Lights />
+      <Shadow />
+      {/* <SphereMesh
+        position={[1.5, 0, 1]}
+        speed={3}
+        link={'boxonelink'}
+        name={'one'}
+      />
+      <SphereMesh
+        position={[-1.4, 3.3, 0.8]}
+        speed={3}
+        link={'boxtwolink'}
+        name={'two'}
+      />
+      <SphereMesh
+        position={[0.5, 3, -1.5]}
+        speed={3}
+        link={'boxthreelink'}
+        name={'three'}
+      /> */}
+      <Tree />
+      <OControls />
+    </group>
+  );
+};
+
 const Three = () => {
+  THREE.LinearFilter;
   return (
     <Canvas
+      className="c"
       shadowMap
       colorManagement
-      camera={{ position: [0, 20, 0], fov: 25 }}
+      camera={{ position: [1, 1, 1], fov: 10 }}
+      // onCreated={({ scene }) => {
+      //   scene.background = new THREE.TextureLoader().load(
+      //     '/images/whitetpaper.png'
+      //   );
+      // }}
+      pixelRatio={2}
     >
       <Suspense fallback={null}>
-        <Lights />
-        <group position={[0, 0, 0]}>
-          <mesh
-            receiveShadow
-            rotation={[-Math.PI / 2, 0, 0]}
-            position={[0, -3, 0]}
-          >
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach="material" opacity={0.3} />
-          </mesh>
-          <SphereMesh
-            position={[1.5, 0, 1]}
-            speed={3}
-            link={'boxonelink'}
-            name={'one'}
-          />
-          <SphereMesh
-            position={[-1.4, 0.3, 0.8]}
-            speed={3}
-            link={'boxtwolink'}
-            name={'two'}
-          />
-          <SphereMesh
-            position={[0.5, 0, -1.5]}
-            speed={3}
-            link={'boxthreelink'}
-            name={'three'}
-          />
-          <Tree />
-        </group>
-        <OrbitControls
-          // enableZoom={false}
-          // enableKeys={true}
-          minPolarAngle={Math.PI / 2 - 0.2}
-          maxPolarAngle={Math.PI / 2}
-          autoRotate
-        />
+        <Group />
       </Suspense>
     </Canvas>
   );
 };
 
 export default Three;
-
-/* LOAD AN GTLF
-import { useGLTF } from '@react-three/drei';
-const gtlf = useGLTF('/treeOne.gltf', true);
-<primitive object={gtlf.scene} dispose={null} /> */
-
-/* USE OBJ AS HREF:
-import Router from 'next/router';
-Router.push('/link'); */
-
-/* ANIMATE FROM FIRST FRAME TO LAST FRAME ON CLICK:
-const [mixer] = useState(() => new THREE.AnimationMixer());
-let animation = mixer
-  .clipAction(gltf.animations[0], group.current)
-  .setLoop(THREE.LoopOnce);
-animation.clampWhenFinished = true;
-animation.enable = true;
-animation.play(); */
-
-/* LIMIT Y AXIS ORBIT CONTROLS AND AUTO ROTATE:
-<OrbitControls
-  enableZoom={false}
-  enableKeys={true}
-  minPolarAngle={Math.PI / 2 - 0.5}
-  maxPolarAngle={Math.PI / 2}
-  autoRotate
-/>; */
-
-/* CHANGE CURSOR TO POINTER ON OBJECT HOVER
-const handleHover = (cursor) => {
-  cursor
-    ? (document.body.style.cursor = 'pointer')
-    : (document.body.style.cursor = 'default');
-};
-  <mesh
-    onPointerOver={() => handleHover(true)}
-    onPointerOut={() => handleHover(false)}
-  > */
-
-/* ONLY CLICK ON MESH CLOSEST TO THE CAMERA
-  e.stopPropagation();
-
-HOW TO SET AND INTEVAL IN REACT
-useEffect(() => {
-  const interval = setInterval(() => {
-    console.log('This will run every second!');
-  }, 1000);
-  return () => clearInterval(interval);
-}, []); */
-
-// PLAY ANIMATION ON CLICK
-
-// TRIGGER AUDIO ON CLICK
-
-// TRIGGER ORBIT CONTROLS ANIMATION ON CLICK
-
-// ATTACH HTML TO OBJECT
-// SHITTY WAY
-//  <Html center>
-//   <p
-//     onClick={(e) => handleClick(e)}
-//     onPointerOver={(e) => handleHover(e, true)}
-//     onPointerOut={(e) => handleHover(e, false)}
-//   >
-//     {name}
-//   </p>
-// </Html>
-
-// COLLAPSE CANVAS ON LINK CLICK
-
-// PHYSICS ON LEAVES ON HOVER
-
-// ANIMATE WALNUT ON HOVER
-
-// clicked ? (z = 80) : (z = 10),
-// from: {
-//   z: 80,
-// },
-// z: 10,
-// onFrame: ({ z }) => {
-//   camera.position.z = z;
-// },
-
-// const springRef = useRef();
-// const { size, opacity, ...rest } = useSpring({
-//   ref: springRef,
-//   from: { size: '20%', background: 'hotpink' },
-//   to: { size: open ? '100%' : '20%', background: open ? 'white' : 'hotpink' },
-// });
--- three-js-tutorial/three/tree.js
+++ three-js-tutorial/three/tree.js
@@ -1,13 +1,32 @@
+// // Three
+// import { useGLTF } from '@react-three/drei';
+
+// const Tree = () => {
+//   const gtlf = useGLTF('/treeOne.gltf');
+//   return (
+//     <mesh position={[0, 0, 0]} scale={[0.8, 0.8, 0.8]} receiveShadow castShadow>
+//       <bufferGeometry attach="geometry" />
+//       <meshStandardMaterial attach="material" />
+//       <primitive object={gtlf.scene} dispose={null} />;
+//     </mesh>
+//   );
+// };
+
+// export default Tree;
+
 // Three
+import { useThree } from 'react-three-fiber';
 import { useGLTF } from '@react-three/drei';
 
 const Tree = () => {
-  const gtlf = useGLTF('/treeOne.gltf');
+  const gtlf = useGLTF('/sapling-v4.gltf');
+  const { camera } = useThree();
+  console.log(camera);
   return (
-    <mesh castShadow position={[0, -3, 0]} scale={[0.8, 0.8, 0.8]}>
+    <mesh position={[0, -0.75, 0]}>
+      {/* <bufferGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" /> */}
       <primitive object={gtlf.scene} dispose={null} />;
-      <bufferGeometry attach="geometry" />
-      <meshStandardMaterial attach="material" />
     </mesh>
   );
 };
